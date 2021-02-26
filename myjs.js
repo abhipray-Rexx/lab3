948c91c4dd7394da38a0a0f122ffd081265f29f6
abhipray-Rexx <78963559+abhipray-Rexx@users.noreply.github.com>
2021-02-26 10:33:05 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,12 @@
+function fir(){
+document.getElementById("dispframe").innerHTML="<iframe class='f1' src='para.html' ></iframe>";
+}
+function sec(){
+    document.getElementById("dispframe").innerHTML="<iframe class='f1' src='lists.html' ></iframe>";
+}
+function fir1(){
+    document.getElementById("dispframe2").innerHTML="<iframe class='f2' src='para2.html' ></iframe>";
+    }
+function sec1(){
+        document.getElementById("dispframe2").innerHTML="<iframe class='f2' src='lists2.html' ></iframe>";
+    }
